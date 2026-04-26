46a1c88ea483ea7e63107afd2d6d5962907a6b85
Jun-Murakami <contact@bucketrelay.com>
2026-04-26 20:25:33 +0900

    Remove build artifacts and configuration files for Linux, including .ninja_deps, .ninja_log, build.ninja, cmake_install.cmake, CMakeCache.txt, and other related files to clean up the build directory.

--- build-linux/clap-juce-extensions/clapjuceext_clap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Synching/code/JUCE/TinyVU/clap-juce-extensions/clap-libs/clap
-# Build directory: /mnt/d/Synching/code/JUCE/TinyVU/build-linux/clap-juce-extensions/clapjuceext_clap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
